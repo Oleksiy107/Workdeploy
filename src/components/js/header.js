f8f3321ca956b8cf8635ca23bb699b66b389cb11
Oleksiy Asiuchenko <asucenkooleksij@gmail.com>
2023-11-10 13:58:25 +0200

    Updates

--- src/components/js/header.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
-  let menu = document.getElementById("menu");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-});
